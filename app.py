7e4be9d2a9b9230683dad37015aff920916e2f7e
LGtechaz <vatslavyjandrij@gmil.com>
2024-10-16 16:38:22 +0300

    add config.py

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-from flask import Flask 
-app = Flask (__name__)
+from flask import Flask, request, redirect, url_for
 
+app = Flask (__name__)
+app.config.from_pyfile("config.py")
 @app.route('/')
 def main():
-    return 'Hello, world!', 200
+    return '<h1>Hello, world!</h1>', 200
 
 @app.route('/homepage') 
 def home():
     """View for the Home page of your website."""
+    agent = request.user_agent
 
-    return "This is your homepage :) "
+    return f"This is your homepage :) - {agent}"
 
+@app.route("/hi/<string:name>/<int:age>")
+def greetings(name):
+    name = name.upper()
+    age=request.args.get("age", 0, int)
+    year = 2024 - age
+    return f"Welcome, {name} - {age}"
+
+@app.route("/admin")
+def admin():
+    to_url = url_for("greetings", name="administrator", age=45, _external=True)     # "http://localhost:8080/hi/administrator?age=45"
+    print(to_url)
+    return redirect(to_url)
+     
 if __name__ == '__main__':
     app.run(debug=True)
